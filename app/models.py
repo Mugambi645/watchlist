0be50b4a0dc772d92fe9210c8d6805c4071642c5
mugambi645 <mugambi645@gmail.com>
2021-11-02 14:54:03 +0300

    added login layout

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import check_password_hash, generate_password_hash
 from . import db
 class Movie:
     """
@@ -19,6 +20,19 @@ class User(db.Model):
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
     pass_secure = db.Column(db.String(255))
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f"User {self.username}"
