d7141eb84bd79aaee7ca23a4a2c19866092e9879
mugambi645 <mugambi645@gmail.com>
2021-11-02 11:42:29 +0300

    added postgres configurations

--- app/config.py
+++ app/config.py
@@ -4,9 +4,12 @@ class Config:
     """
     General configuration class
     """
-    
+
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
-    
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pato:flower2@localhost/watchlist'
+
 class ProdConfig(Config):
     """
     Production configuration child class
